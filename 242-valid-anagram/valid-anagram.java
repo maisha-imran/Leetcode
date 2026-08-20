af3f8620314bba0d2a67f5505611175adaccf50d
Maisha Imran <maishaimran67@gmail.com>
2026-08-20 14:27:53 +0530

    Time: 2 ms (99.65%) | Memory: 44.6 MB (60.46%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public boolean isAnagram(String s, String t) {
         if( s.length() != t.length())
             return false;
@@ -9,6 +9,23 @@ class Solution {
         for( int j=0; j<t.length() ; j++){
             freq[t.charAt(j) - 'a']--;
         }
+        for( int x : freq){
+            if( x !=0)
+                return false;
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if( s.length() != t.length())
+            return false;
+        int freq[] = new int[26];
+        for( char ch : s.toCharArray())
+            freq[ch - 'a']++;
+        for( char ch : t.toCharArray())
+            freq[ch - 'a']--;
         for( int x : freq){
             if( x !=0)
                 return false;
