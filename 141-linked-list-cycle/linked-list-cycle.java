d1769511952e60d6c883f33a6e1b07564992c55c
Maisha Imran <maishaimran67@gmail.com>
2026-08-22 03:53:59 +0530

    Time: 8 ms (9.68%) | Memory: 46.9 MB (16.35%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -1,24 +1,17 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow= head;
-        ListNode fast = head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-            if(slow==fast)
-            return true;
+        ListNode temp= head;
+        HashMap<ListNode , Integer> map = new HashMap<>();
+        while(temp!=null){
+            if(map.containsKey(temp))
+                return true;
+        map.put(temp , 1);
+        temp=temp.next;
         }
-        return false;
+    return false;
     }
 }
+
+
