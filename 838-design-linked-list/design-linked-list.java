8fd170f2b2b7f1660594a8df9bc34d682ca6c63a
Maisha Imran <maishaimran67@gmail.com>
2026-08-22 01:48:30 +0530

    Time: 11 ms (43.81%) | Memory: 47.2 MB (18.82%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.java
@@ -0,0 +1,98 @@
+class MyLinkedList {
+    // Node class
+    class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+            this.next = null;
+        }
+
+        Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    Node head;
+    public MyLinkedList() {
+        head = null;
+    }
+    public int get(int index) {
+        Node temp = head;
+        int c=0;
+        while( temp != null){
+            if( c == index)
+                return temp.val;
+            temp = temp.next;
+            c++;
+        }
+        return -1;
+    }
+    
+    public void addAtHead(int val) {
+        head = new Node( val , head);
+    }
+    
+    public void addAtTail(int val) {
+        if( head == null){
+            head = new Node( val , null);
+            return;
+        }
+        Node temp = head;
+        while( temp.next != null){
+            temp = temp.next;
+        }
+        Node naya = new Node(val , null);
+        temp.next = naya;
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if( index == 0){
+            addAtHead(val);
+            return;
+        }
+        Node temp = head;
+        int c=0;
+        while(temp !=null){
+            if( c == index -1)
+                break;
+            temp = temp.next;
+            c++;
+        }
+        if( temp ==null)
+            return;
+        Node naya = new Node( val , temp.next);
+        temp.next = naya;
+    }
+    
+    public void deleteAtIndex(int index) {
+        if( head == null )
+            return;
+        if ( index == 0){
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        int c=0;
+        while( temp != null){
+            if ( c==index-1)
+                break;
+            temp = temp.next;
+            c++;
+        }
+        if (temp == null || temp.next == null)
+            return;
+        temp.next = temp.next.next;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
