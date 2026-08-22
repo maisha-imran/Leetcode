4894ce0e4a227146a6c29059b90ad87f7b033bac
Maisha Imran <maishaimran67@gmail.com>
2026-08-22 15:16:50 +0530

    Time: 10 ms (14.08%) | Memory: 91.6 MB (99.89%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,48 +8,68 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution1 {
+class Solution2 {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow=head,fast=head;
-        if(fast.next==null)
-        return true;
-        while(fast!=null && fast.next!=null){
-            slow= slow.next;
-            fast= fast.next.next;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        ListNode temp=slow;
-        ListNode prev=null;
-               while(slow!=null){
-                temp=slow.next;
-                slow.next=prev;
-                prev=slow;
-                slow=temp;
-               }
-            fast=head;
-            while(prev!=null){
-                if(prev.val!=fast.val)
+        ListNode temp = slow;
+        ListNode prev = null;
+        while (slow != null) {
+            temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        fast = head;
+        while (prev != null) {
+            if (prev.val != fast.val)
                 return false;
-                prev = prev.next;
-                fast = fast.next;
-            }
-            return true; 
-            }
+            prev = prev.next;
+            fast = fast.next;
         }
+        return true;
+    }
+}
 
-class Solution {
+class Solution1 {
     public boolean isPalindrome(ListNode head) {
         Stack<Integer> st = new Stack<>();
         ListNode temp = head;
-        while( temp != null){
+        while (temp != null) {
             st.add(temp.val);
             temp = temp.next;
         }
         temp = head;
-        while( !(st.isEmpty())){
-            if( temp.val != st.pop())
+        while (!(st.isEmpty())) {
+            if (temp.val != st.pop())
                 return false;
             temp = temp.next;
         }
         return true;
     }
-} 
+}
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+        Stack <Integer> st = new Stack<>();
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        fast = head;
+        while( slow != null){
+            st.add(slow.val);
+            slow = slow.next;
+        }
+        while( !(st.isEmpty())){
+            if( fast.val != st.pop())
+                return false;
+            fast = fast.next;
+        }
+        return true;
+    }
+}
