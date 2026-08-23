688caad1d455d01d03024dd58e0d506753a14a1a
Maisha Imran <maishaimran67@gmail.com>
2026-08-24 02:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (37.08%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -1,11 +1,23 @@
-class Solution {
+class Solution1 {
     public int minBitFlips(int start, int goal) {
-        int c =0;
+        int c = 0;
         int diff = start ^ goal;
-        while ( diff != 0){
-            if( (diff & 1) == 1)
+        while (diff != 0) {
+            if ((diff & 1) == 1)
                 c++;
             diff >>= 1;
+        }
+        return c;
+    }
+}
+
+class Solution {
+public int minBitFlips(int start, int goal) {
+        int c = 0;
+        int diff = start ^ goal;
+        while ( diff != 0){
+            diff = diff & ( diff - 1);
+            c++;
         }
         return c;
     }
