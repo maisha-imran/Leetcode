dcf67150723236888300241575f7a0b88f4be748
Maisha Imran <maishaimran67@gmail.com>
2026-08-24 03:30:56 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (66.35%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,14 +1,25 @@
-class Solution {
+class Solution1 {
     public int missingNumber(int[] nums) {
         Arrays.sort(nums);
-        int i = 0, c=0;
-        for(int num : nums){
+        int i = 0, c = 0;
+        for (int num : nums) {
             c = i ^ num;
-            if( c != 0)
+            if (c != 0)
                 break;
-            else 
+            else
                 i++;
         }
         return i;
+    }
+}
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans ^= i;
+            ans ^= nums[i];
+        }
+        return ans ^ nums.length;
     }
 }
