75e5b9ef6b808e9209eb5210c232a705ca726ee9
Maisha Imran <maishaimran67@gmail.com>
2026-08-27 02:04:21 +0530

    Time: 1 ms (99.87%) | Memory: 69.1 MB (84.24%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -21,10 +21,8 @@ class Solution {
         int l = 0 , r = 0 , sum = 0;
         int len = Integer.MAX_VALUE;
         while( r < nums.length){
-            if( sum < target){
                 sum += nums[r];
                 r++;
-            }
             while( sum >= target){
                 len = Math.min( len , r - l );
                 sum -= nums[l];
